264e42588d467b887f76e693586491e9c944bf0e
Francisco Martinez <frankmartinez4050@gmail.com>
2022-02-16 19:22:54 -0600

    Partially done

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ stocks }) {
+  // Map through all stocks and render them using the Stock component
+  const stocksDisplayed = stocks.map((stock) => {
+    return <Stock key={stock.key} stock={stock} />
+  })
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stocksDisplayed}
     </div>
   );
 }
